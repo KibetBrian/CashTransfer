ff3ffc00e44e814e5325026e068f3310277fdd33
Kibet Brian <briankibet2040@gmail.com>
2022-03-02 04:14:16 +0300

    Refactor

--- api/server.go
+++ api/server.go
@@ -4,21 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Server struct {
-	Router *gin.Engine
-}
-
-func  (s *Server) Serve(port string) *Server{
-	server := &Server{
-		Router: gin.Default(),
-	}
-	AcccountRoutes(server.Router)
-	TransactionRoutes(server.Router)
-	UserRoutes(server.Router)
-	s.Start(port)	
-	return server
-}
-
-func (server *Server) Start(port string) error{
-	return server.Router.Run(port)
+func RegisterRoutes (){
+	router := gin.Default()
+	AcccountRoutes(router)
+	TransactionRoutes(router)
+	UserRoutes(router)
+	router.Run()
 }
--- main.go
+++ main.go
@@ -6,19 +6,14 @@ import (
 	"github.com/KibetBrian/fisa/api"
 	"github.com/KibetBrian/fisa/configs"
 	"github.com/KibetBrian/fisa/models"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
-	 server := &api.Server{
-		 Router: gin.Default(),
-	 }
-	 server.Serve("localhost:8080")
-	
-	db, err := configs.ConnectDb() 
-	if err != nil{
-		log.Fatal("Database connection failed. Err: ",err)
+
+	api.RegisterRoutes()
+	db, err := configs.ConnectDb()
+	if err != nil {
+		log.Fatal("Database connection failed. Err: ", err)
 	}
 	db.AutoMigrate(&models.User{}, &models.Account{}, &models.Transaction{})
 
