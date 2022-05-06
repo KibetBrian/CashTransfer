51c7be2256d8ced42b83af18fb3f8041e7e70a3c
Kibet Brian <briankibet2040@gmail.com>
2022-05-07 00:24:19 +0300

    refactor

--- services/transaction_test.go
+++ services/transaction_test.go
@@ -1,10 +1,9 @@
-package tests
+package services
 
 import (
 	"testing"
 
 	"github.com/KibetBrian/fisa/models"
-	"github.com/KibetBrian/fisa/services"
 	"github.com/KibetBrian/fisa/utils"
 	"github.com/satori/go.uuid"
 	"github.com/shopspring/decimal"
@@ -15,10 +14,10 @@ func TestTransaction(t *testing.T){
 	accountId1, _ := uuid.FromString("d04253b5-e484-4874-886a-b1b471b64b6b")
 	accountId2, _ := uuid.FromString("447e1d99-7992-4442-8330-ae9620e2f0ad")
 
-	account1, success:= services.GetAccount(accountId1)
+	account1, success:= GetAccount(accountId1)
 	require.True(t, success)
 
-	account2, success := services.GetAccount(accountId2)
+	account2, success := GetAccount(accountId2)
 	require.True(t, success)
 
 	routines := 10;
@@ -29,7 +28,7 @@ func TestTransaction(t *testing.T){
 
 	for i:=0; i<routines; i++{
 		go func(){
-			transaction, res, isSuccessful :=services.DoubleEntry(accountId1, accountId2, decimal.NewFromInt32(amount))
+			transaction, res, isSuccessful :=DoubleEntry(accountId1, accountId2, decimal.NewFromInt32(amount))
   			isSuccessfulChan <- isSuccessful
 			transactionChan <- transaction;
 			stringRes <-res
