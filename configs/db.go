623e7175462e2b7e16752737415775704b7467a2
KibetBrian <briankibet2040@gmail.com>
2022-09-30 23:44:11 +0300

    update env value retrieval

--- configs/db.go
+++ configs/db.go
@@ -13,36 +13,45 @@ import (
 )
 
 var (
-	host = os.Getenv("DB_HOST")
-	port=os.Getenv("DB_PORT")
-	user=os.Getenv("DB_USER")
-	password=os.Getenv("DB_PASSWORD")
-	database=os.Getenv("DB_DATABASE")
+	envKeys = []string{"DB_HOST","DB_PORT", "DB_USER", "DB_PASSWORD", "DB_NAME"}
+	envValues = map[string]string{
+		"DB_HOST": "",
+        "DB_PORT": "",
+        "DB_USER": "",
+        "DB_PASSWORD": "",
+        "DB_NAME": "",
+	}
 	Db          *gorm.DB
 )
 
-func checkError(err error) bool {
-	if err != nil {
-		return err != nil
+
+func FetchEnvValues(){
+	for _, key := range envKeys {
+        value, ok := os.LookupEnv(key);
+		if !ok {
+			log.Fatalf("Failed to get environment variable%v", key)
+		}
+		envValues[key] = value
 	}
-	return false
 }
 
 func ConnectDb() (*gorm.DB, error) {
+	FetchEnvValues()
 
 	//Converts string port from env to port number
-	var _, err = strconv.Atoi(port)
+	var _, err = strconv.Atoi(envValues["DB_PORT"])
 	if err != nil {
-		log.Fatal("Error converting string port to int")
+		log.Fatalf("Error converting string port to int. Error: %v", err)
 	}
 
 	//Postgres Connection details
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", envValues["DB_HOST"], envValues["DB_PORT"], envValues["DB_USER"], envValues["DB_PASSWORD"], envValues["DB_NAME"])
 
 	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
-	if checkError(err) {
-		return nil, err
+	if err !=nil {
+		log.Fatalf("Database connection error: %v", err)
 	}
+
 	//Auto migrations
 	db.AutoMigrate(&models.User{}, &models.Account{}, &models.Transaction{})
 	return db, nil
